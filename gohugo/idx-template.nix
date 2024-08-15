6823af8044393d7c5507fc9d92b09a290b8241e5
v01p3 <115195129+v01p3@users.noreply.github.com>
2024-08-15 17:12:02 +0200

    Update idx-template.nix

--- gohugo/idx-template.nix
+++ gohugo/idx-template.nix
@@ -4,16 +4,13 @@
   pkgs.hugo
   ];
   bootstrap = ''
-    mkdir -p "$out"
-    chmod -R +w "$out"
-
-    mkdir -p "$out"/.idx
-    cp -rf ${./dev.nix} "$out"/.idx/dev.nix
-    cp -rf ${./icon.png} "$out"/.idx/icon.png
-
     hugo new site "$out"
     cp -rf ${./blog_simple} "$out"/themes/blog_simple
     cp -rf ${./hugo.toml} "$out"/hugo.toml
     cp -rf ${./content} "$out"/content
+
+    mkdir -p "$out"/.idx
+    cp -rf ${./dev.nix} "$out"/.idx/dev.nix
+    cp -rf ${./icon.png} "$out"/.idx/icon.png
   '';
 }
