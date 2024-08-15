0b9746a65bd561a5a56684edff1eebab8018087e
v01p3 <115195129+v01p3@users.noreply.github.com>
2024-08-15 09:36:13 +0200

    Update dev.nix

--- gohugo/dev.nix
+++ gohugo/dev.nix
@@ -1,4 +1,3 @@
-# .idx/dev.nix
 {pkgs}: {
   channel = "stable-23.11";
   packages = [
@@ -6,21 +5,6 @@
     pkgs.hugo
   ];
   idx = {
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-        # Open editors for the following files by default, if they exist.
-        # The last file in the list will be focused.
-        default.openFiles = [
-          
-        ];
-        # Include other scripts here, as needed, for example:
-        # installDependencies = "npm install";
-      };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
-    };
-    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
