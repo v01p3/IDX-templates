5073f7d4707e6313207306e7d3af3128d3ece1d2
v01p3 <115195129+v01p3@users.noreply.github.com>
2024-08-15 09:37:46 +0200

    Update idx-template.nix

--- gohugo/idx-template.nix
+++ gohugo/idx-template.nix
@@ -1,26 +1,16 @@
-# No user-configurable parameters
 { pkgs, ... }: {
   packages = [
   pkgs.go
   pkgs.hugo
   ];
-  # Shell script that produces the final environment
   bootstrap = ''
     mkdir "$out"
-    # Copy the folder containing the `idx-template` files to the final
-    # project folder for the new workspace. ${./.} inserts the directory
-    # of the checked-out Git folder containing this template.
     hugo new site "$out"
     cp -r ${./blog_simple} "$out"/themes/blog_simple
     cp -r ${./hugo.toml} "$out"/hugo.toml
     cp -r ${./content} "$out"/content
     mkdir -p "$out"/.idx
     cp ${./dev.nix} "$out"/.idx/dev.nix
-
-    # Set some permissions
     chmod -R +w "$out"
-
-    # Remove the template files themselves and any connection to the template's
-    # Git repositor
   '';
 }
