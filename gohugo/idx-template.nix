0a6ad0163851671496df67cfa54f3ec5f9313655
v01p3 <115195129+v01p3@users.noreply.github.com>
2024-08-15 13:09:59 +0200

    Update idx-template.nix

--- gohugo/idx-template.nix
+++ gohugo/idx-template.nix
@@ -2,6 +2,7 @@
   packages = [
   pkgs.go
   pkgs.hugo
+  pkgs.nodejs_20
   ];
   bootstrap = ''
     mkdir "$out"
@@ -12,6 +13,7 @@
     mkdir -p "$out"/.idx
     cp ${./dev.nix} "$out"/.idx/dev.nix
     cp ${./icon.png} "$out"/.idx/icon.png
+    cd "$out"/themes/blog_simple; npm init -y && npm install -D tailwindcss && npx tailwindcss init
     chmod -R +w "$out"
   '';
 }
