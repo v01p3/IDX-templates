3561dcebd4014c8d2aaa4edbc84b4407bb45d0c1
v01p3 <115195129+v01p3@users.noreply.github.com>
2024-08-15 09:25:00 +0200

    Update idx-template.nix

--- gohugo/idx-template.nix
+++ gohugo/idx-template.nix
@@ -12,6 +12,7 @@
     # of the checked-out Git folder containing this template.
     hugo new site "$out"
     cp -r ${./blog_simple} "$out"/themes/blog_simple
+    cp -r ${./hugo.toml} "$out"/hugo.toml
     cp -r ${./content} "$out"/content
     mkdir -p "$out"/.idx
     cp ${./dev.nix} "$out"/.idx/dev.nix
